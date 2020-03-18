347f3b58ef38d0d4d73e36b28bb455b8eae309a0
Celtia <celtiajuan@hotmail.com>
2020-03-18 17:22:30 +0100

    Initial commit

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    public float velocidad = 0.1f;
+    public bool tonta;
+    public GameObject socket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movx = Input.GetAxis("Horizontal")*velocidad + transform.position.x;
+        float movy = Input.GetAxis("Vertical") * velocidad + transform.position.y;
+        transform.position = new Vector2(movx, movy);
+      
+       
+    }
+   public Vector2 get_socket()
+    {
+        return socket.transform.position;
+    }
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+        //barco -> tiene una clase que se llama set_pasajero(gameobject)
+        //disparo tiene una referencia al arma del que ha salido
+        if (collision.tag.Equals("Barco"))
+        {
+            collision.GetComponent<Barco>().set_pasajero(this);
+        }
+        if (collision.tag.Equals("Disparo_enemigo"))
+        {
+            GameObject aux= collision.GetComponent<Disparo>().get_arma();
+            vida= vida - aux.GetComponent<Arma>.get_daño();
+
+        }
+    } */
+}
---
+++ Assets/Scripts/Objeto.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objeto : MonoBehaviour
+{
+    private bool cogido = false;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cogido)
+        {
+           transform.position = player.GetComponent<Movimiento>().get_socket();
+            if (Input.GetKeyDown(KeyCode.Q)){
+                cogido = false;
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!cogido) { 
+           if ((collision.tag.Equals("Player"))&& (Input.GetKeyDown(KeyCode.E)))
+        {
+            cogido = true;
+            transform.position = collision.GetComponent<Movimiento>().get_socket();
+            player = collision.gameObject;
+        }
+    }
+    }
+   
+}
